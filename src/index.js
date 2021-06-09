69ef889e83262ec01a57280d68427b0f6317e6f3
tharunsamael <84525292+tharunsamael@users.noreply.github.com>
2021-06-09 09:19:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
-import { HashRouter, Route, Switch } from "react-router-dom";
-import { createBrowserHistory } from "history";
-import { render } from "react-dom";
+
 import "./index.css";
 import { App } from "./Signup/root";
-import Signupbox from "./Signup/Signupbox";
-import Loginbox from "./Login/Loginbox";
 import React from "react";
+immport ReactDOM from "react-dom";
 
-class Apps extends React.Component {
-  render() {
-    return (
-      <HashRouter basename="/" history={createBrowserHistory() 
-        <Switch
-          <Route path={"/"} component={App} exact={true} />
-          <Route path={"/signup"} component={Signupbox} />
-          <Route path={"/login"} component={Loginbox} />
-        />
-      />
-    );
-  }
-}
 
-render(<Apps />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
