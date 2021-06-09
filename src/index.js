f2142aa04eda581078a0114e1a0034811dc9d9a0
tharunsamael <84525292+tharunsamael@users.noreply.github.com>
2021-06-09 09:11:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import { render } from "react-dom";
 import "./index.css";
@@ -10,13 +10,13 @@ import React from "react";
 class Apps extends React.Component {
   render() {
     return (
-      <Router history={createBrowserHistory() >
-        <Switch>
-          <Route path={process.env.PUBLIC_URL + "/"} component={App} exact={true}></Route>
-          <Route path={"/signup"} component={Signupbox}></Route>
-          <Route path={"/login"} component={Loginbox}></Route>
-        </Switch>
-      </Router>
+      <HashRouter basename="/" history={createBrowserHistory() 
+        <Switch
+          <Route path={process.env.PUBLIC_URL + "/"} component={App} exact={true} />
+          <Route path={"/signup"} component={Signupbox} />
+          <Route path={"/login"} component={Loginbox} />
+        />
+      />
     );
   }
 }
